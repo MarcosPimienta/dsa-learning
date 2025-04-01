bbce96b90fbbf86b7cf56422e9f4c0cb24e764a4
MarcosPimienta <fenix3819@gmail.com>
2025-03-31 21:40:36 -0500

    added a single linked list

---
+++ data_structures/linked_lists/single_list.py
@@ -0,0 +1,65 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+        
+class s_linked_list:
+    def __init__(self):
+        self.head = None
+        return
+    
+    def add_end(self, data):
+
+        if data is None:
+            return
+
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+
+        current = self.head
+
+        while current.next:
+            current = current.next
+        current.next = new_node
+    
+    def add_nodes_list(self, values):
+        
+        if values:
+            for value in values:
+                self.add_end(value)
+
+    def traverse_f(self, node=None, nodes=None):
+        if nodes is None:
+            nodes = []
+
+        if node:
+            nodes.append(node.data)
+            self.traverse_f(node.next, nodes)
+
+        return nodes
+
+    def reverse(self):
+        prev = None
+        current = self.head
+
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+
+        self.head = prev
+
+nodes = [7, 12, 15]
+linked_list = s_linked_list()
+
+linked_list.add_nodes_list(nodes)
+l = linked_list.traverse_f(linked_list.head)
+print(f"{l}")
+
+linked_list.reverse()
+
+print("After reverse:", linked_list.traverse_f(linked_list.head))
